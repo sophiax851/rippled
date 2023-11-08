9fff44284c0d44253667667c12b995c80b93340f
Bronek Kozicki <brok@incorrekt.com>
2023-11-08 18:36:24 +0000

    Unify JSON serialization format of transactions (#4775)
    * Remove include <ranges>
    
    * Formatting fix
    
    * Output for subscriptions
    
    * Output from sign, submit etc.
    
    * Output from ledger
    
    * Output from account_tx
    
    * Output from transaction_entry
    
    * Output from tx
    
    * Store close_time_iso in API v2 output
    
    * Add small APIv2 unit test for subscribe
    
    * Add unit test for transaction_entry
    
    * Add unit test for tx
    
    * Remove inLedger from API version 2
    
    * Set ledger_hash and ledger_index
    
    * Move isValidated from RPCHelpers to LedgerMaster
    
    * Store closeTime in LedgerFill
    
    * Time formatting fix
    
    * additional tests for Subscribe unit tests
    
    * Improved comments
    
    * Rename mInLedger to mLedgerIndex
    
    * Minor fixes
    
    * Set ledger_hash on closed ledger, even if not validated
    
    * Update API-CHANGELOG.md
    
    * Add ledger_hash, ledger_index to transaction_entry
    
    * Fix validated and close_time_iso in account_tx
    
    * Fix typos
    
    * Improve getJson for Transaction and STTx
    
    * Minor improvements
    
    * Replace class enum JsonOptions with struct
    
    We may consider turning this into a general-purpose template and using it elsewhere
    
    * simplify the extraction of transactionID from Transaction object
    
    * Remove obsolete comments
    
    * Unconditionally set validated in account_tx output
    
    * Minor improvements
    
    * Minor fixes
    
    ---------
    
    Co-authored-by: Chenna Keshava <ckeshavabs@gmail.com>

--- src/ripple/rpc/impl/TransactionSign.cpp
+++ src/ripple/rpc/impl/TransactionSign.cpp
@@ -646,12 +646,20 @@ transactionConstructImpl(
 }
 
 static Json::Value
-transactionFormatResultImpl(Transaction::pointer tpTrans)
+transactionFormatResultImpl(Transaction::pointer tpTrans, unsigned apiVersion)
 {
     Json::Value jvResult;
     try
     {
-        jvResult[jss::tx_json] = tpTrans->getJson(JsonOptions::none);
+        if (apiVersion > 1)
+        {
+            jvResult[jss::tx_json] =
+                tpTrans->getJson(JsonOptions::disable_API_prior_V2);
+            jvResult[jss::hash] = to_string(tpTrans->getID());
+        }
+        else
+            jvResult[jss::tx_json] = tpTrans->getJson(JsonOptions::none);
+
         jvResult[jss::tx_blob] =
             strHex(tpTrans->getSTransaction()->getSerializer().peekData());
 
@@ -778,6 +786,7 @@ checkFee(
 Json::Value
 transactionSign(
     Json::Value jvRequest,
+    unsigned apiVersion,
     NetworkOPs::FailHard failType,
     Role role,
     std::chrono::seconds validatedLedgerAge,
@@ -808,13 +817,14 @@ transactionSign(
     if (!txn.second)
         return txn.first;
 
-    return transactionFormatResultImpl(txn.second);
+    return transactionFormatResultImpl(txn.second, apiVersion);
 }
 
 /** Returns a Json::objectValue. */
 Json::Value
 transactionSubmit(
     Json::Value jvRequest,
+    unsigned apiVersion,
     NetworkOPs::FailHard failType,
     Role role,
     std::chrono::seconds validatedLedgerAge,
@@ -854,7 +864,7 @@ transactionSubmit(
             rpcINTERNAL, "Exception occurred during transaction submission.");
     }
 
-    return transactionFormatResultImpl(txn.second);
+    return transactionFormatResultImpl(txn.second, apiVersion);
 }
 
 namespace detail {
@@ -944,6 +954,7 @@ sortAndValidateSigners(STArray& signers, AccountID const& signingForID)
 Json::Value
 transactionSignFor(
     Json::Value jvRequest,
+    unsigned apiVersion,
     NetworkOPs::FailHard failType,
     Role role,
     std::chrono::seconds validatedLedgerAge,
@@ -1044,13 +1055,14 @@ transactionSignFor(
     if (!txn.second)
         return txn.first;
 
-    return transactionFormatResultImpl(txn.second);
+    return transactionFormatResultImpl(txn.second, apiVersion);
 }
 
 /** Returns a Json::objectValue. */
 Json::Value
 transactionSubmitMultiSigned(
     Json::Value jvRequest,
+    unsigned apiVersion,
     NetworkOPs::FailHard failType,
     Role role,
     std::chrono::seconds validatedLedgerAge,
@@ -1237,7 +1249,7 @@ transactionSubmitMultiSigned(
             rpcINTERNAL, "Exception occurred during transaction submission.");
     }
 
-    return transactionFormatResultImpl(txn.second);
+    return transactionFormatResultImpl(txn.second, apiVersion);
 }
 
 }  // namespace RPC
